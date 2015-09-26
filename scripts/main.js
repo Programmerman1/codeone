a228477c5d5bb203e5c5194a3899de8652dafe33
Marcus Ross <marcus.ross@gmail.com>
2015-09-25 21:50:33 -0500

    adding tablet friendly number adjuster example.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
-﻿var load = function() {
+﻿var load = function () {
     console.log('hi folks');
 }();
+var adjustAmount = function (targetFieldId) {
+    var field = document.getElementById(targetFieldId);
+    var amount = parseInt((event.target || event.srcElement).dataset.amount);
+    field.value = parseInt(field.value) + amount;
+    event.stopPropagation();
+};
