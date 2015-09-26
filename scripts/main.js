22db26e0710df280ef435cbec5ef86d27f2283d3
Marcus Ross <marcus.ross@gmail.com>
2015-09-26 11:18:10 -0500

    moving things about

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,6 @@
 var adjustAmount = function (targetFieldId) {
     var field = document.getElementById(targetFieldId);
     var amount = parseInt((event.target || event.srcElement).dataset.amount);
-    field.value = parseInt(field.value) + amount;
+    field.value = (parseInt(field.value) || 0) + amount;
     event.stopPropagation();
 };
