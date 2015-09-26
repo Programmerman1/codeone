3cf34c871f2bfbdf670bdf70575e6422b6f93f60
Nick Allmaker <programmerman1@aol.com>
2015-09-26 12:47:50 -0500

    Making it so that if a field would go negative instead it doesn't.

--- scripts/main.js
+++ scripts/main.js
@@ -5,5 +5,6 @@ var adjustAmount = function (targetFieldId) {
     var field = document.getElementById(targetFieldId);
     var amount = parseInt((event.target || event.srcElement).dataset.amount);
     field.value = (parseInt(field.value) || 0) + amount;
+    if (field.value < 0) field.value = 0;
     event.stopPropagation();
 };
