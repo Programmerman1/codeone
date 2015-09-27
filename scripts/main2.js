9cf3ba38c149e21cf6a0faecbc867c66344e7d02
Cara Heacock <caraheacock@gmail.com>
2015-09-26 21:41:19 -0500

    So much CSS animation, kill me

--- scripts/main2.js
+++ scripts/main2.js
@@ -5,4 +5,23 @@ jQuery('document').ready(function($){
     $goalIcons.click(function() {
         $(this).parents('.column-inner').toggleClass('active');
     });
+    
+    /* Fixed position calculating bear */
+    var $window = $(window),
+        $survey = $('#survey'),
+        $calculatingBear = $('.calculating-bear');
+    
+    var fixTheBear = function() {
+        var bearOriginalTop = $survey.offset().top,
+            scrollTop = $window.scrollTop();
+            
+        if (scrollTop > bearOriginalTop && !$calculatingBear.hasClass('fixed-bear')) {
+            $calculatingBear.addClass('fixed-bear');
+        } else if (scrollTop <= bearOriginalTop && $calculatingBear.hasClass('fixed-bear')) {
+            $calculatingBear.removeClass('fixed-bear');
+        }
+    };
+    
+    fixTheBear();
+    $window.scroll(fixTheBear);
 });
