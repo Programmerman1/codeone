6e4fc041dfd85ee48edea0a8422e7704b8ec4ae1
Cara Heacock <caraheacock@gmail.com>
2016-06-14 00:26:31 -0500

    Deleted main2.js and added contents to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -147,3 +147,44 @@ $('input[type="radio"]').click(function () {
             $('.' + controlClass).hide("slow");
     }
 });
+
+/* Toggle active state on goals */
+var $goalIcons = $('#goals .bb');
+
+$goalIcons.click(function() {
+    $(this).parents('.column-inner').toggleClass('active');
+});
+
+jQuery('document').ready(function($){
+    /* Fixed position calculating bear */
+    var $window = $(window),
+        $survey = $('#survey'),
+        $calculatingBear = $('.calculating-bear');
+    
+    var fixTheBear = function() {
+        var bearOriginalTop = $survey.offset().top,
+            bearHeight = $calculatingBear.find('.calculating-bear-inner-inner').height(),
+            surveyBottom = bearOriginalTop + $survey.outerHeight(),
+            scrollTop = $window.scrollTop(),
+            threshold = scrollTop + bearHeight + 60;
+            
+        if (scrollTop > bearOriginalTop && !$calculatingBear.hasClass('fixed-bear')) {
+            $calculatingBear.addClass('fixed-bear');
+        } else if (scrollTop <= bearOriginalTop && $calculatingBear.hasClass('fixed-bear')) {
+            $calculatingBear.removeClass('fixed-bear');
+        }
+        
+        if (threshold > surveyBottom) {
+            $calculatingBear.css({
+                'position': 'absolute',
+                'bottom': bearHeight,
+                'top': 'auto'
+            });
+        } else {
+            $calculatingBear.removeAttr('style');
+        }
+    };
+    
+    fixTheBear();
+    $window.scroll(fixTheBear);
+});
